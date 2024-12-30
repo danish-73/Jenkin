6a099041426e2842c3d8a343e7166326d558803c
danish-73 <danishcloud73@gmail.com>
2024-12-31 01:23:28 +0530

    modified the file

--- etl.py
+++ etl.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd 
 
-st.write("Welcome to AVD73")
+st.write("Welcome")
 
 data = {"Task":["Extract","Transform","Load"],
         "Status":["Completed","Inprogress","Pending"] }
